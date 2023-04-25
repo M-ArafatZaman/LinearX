9adcdeabd1895a0db0a334f63e814cfc800a1f8c
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-04-25 13:23:41 -0230

    Added Roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ function App() {
 
     return (
         <div className="flex flex-col h-screen">
-            <header className="bg-blue-500 text-white p-4">
+            <header className="bg-blue-500 text-white p-8">
                 <h1 className="text-2xl font-bold">Header</h1>
             </header>
             <main className="flex-1 p-4">
+                <div className="container mx-auto">
+                    Test
+                </div>
                 <h2 className="text-lg font-bold">Content goes here</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
             </main>
