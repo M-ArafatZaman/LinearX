bfe1f4581f54c8dc9bf54d0c7556543061e346c3
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-04-25 00:50:29 -0230

    Implemented TailwindUI

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import { useState } from 'react';
 function App() {
 
     return (
-        <div className="App">
-            LinearX
+        <div className="flex flex-col h-screen">
+            <header className="bg-blue-500 text-white p-4">
+                <h1 className="text-2xl font-bold">Header</h1>
+            </header>
+            <main className="flex-1 p-4">
+                <h2 className="text-lg font-bold">Content goes here</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+            </main>
+            <footer className="bg-blue-500 text-white p-4">
+                <h3 className="text-xl font-bold">Footer</h3>
+            </footer>
         </div>
     );
 };
