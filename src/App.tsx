4f83defe13d95f5dcea351af239ef50f18a70e46
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-04-25 13:37:57 -0230

    Added a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
+import Logo from './assets/linearx-logo.svg';
 
 function App() {
 
     return (
         <div className="flex flex-col h-screen">
-            <header className="bg-blue-500 text-white p-8">
-                <h1 className="text-2xl font-bold">Header</h1>
+            <header className="p-6 bg-slate-200 text-gray-800">
+                <div className="flex flex-row w-full items-center relative">
+                    <img src={Logo} style={{
+                        maxHeight: "50px",
+                        objectFit: "contain"
+                    }} />
+                    <h1 className="text-2xl font-bold ml-2">LinearX</h1>
+                </div>
             </header>
             <main className="flex-1 p-4">
                 <div className="container mx-auto">
