809bc67d8be37845626715658c53035e4eb697b0
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-04-26 13:17:31 -0230

    Added a font awesome icon

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
                     }} />
                     <h1 className="text-2xl font-bold ml-2">LinearX</h1>
 
-                    <button className="button ml-auto cursor-pointer uppercase font-bold rounded-md shadow-lg px-3 py-2 bg-slate-700 text-white transition-all hover:bg-slate-500 hover:shadow-2xl active:bg-slate-400">Github</button>
+                    <a className="button ml-auto cursor-pointer uppercase font-bold rounded-md shadow-lg px-3 py-2 bg-slate-700 text-white transition-all hover:bg-slate-500 hover:shadow-2xl active:bg-slate-400" href="https://github.com/M-ArafatZaman/LinearX" target="_blank"><i className="fa-brands fa-github mr-1"></i> Github</a>
                 </div>
             </div>
         </header>
