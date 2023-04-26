b016b17606f00f249ad932070638ecb4d20e9284
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-04-26 13:11:19 -0230

    Updated header to have a button

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -5,12 +5,16 @@ const Header: React.FC = () => {
 
     return (
         <header className="p-6 bg-slate-200 text-gray-800">
-            <div className="flex flex-row w-full items-center relative">
-                <img src={Logo} style={{
-                    maxHeight: "50px",
-                    objectFit: "contain"
-                }} />
-                <h1 className="text-2xl font-bold ml-2">LinearX</h1>
+            <div className="container mx-auto">
+                <div className="flex flex-row w-full items-center relative">
+                    <img src={Logo} style={{
+                        maxHeight: "50px",
+                        objectFit: "contain"
+                    }} />
+                    <h1 className="text-2xl font-bold ml-2">LinearX</h1>
+
+                    <button className="button ml-auto cursor-pointer uppercase font-bold rounded-md shadow-lg px-3 py-2 bg-slate-700 text-white transition-all hover:bg-slate-500 hover:shadow-2xl active:bg-slate-400">Github</button>
+                </div>
             </div>
         </header>
     )
