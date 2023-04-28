2692867c868830a749c78a0611632cd096c60d8d
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-04-28 14:04:42 -0230

    Adjusted chart

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Plot from 'react-plotly.js';
 
 const App: React.FC = () => {
 
+    const [height, setHeight] = useState<number>( Math.min( window.innerHeight - 40, 600 ) );
+    const [width, setWidth] = useState<number>( Math.min( window.innerWidth - 20, 900 ) );
+
     return (
         <div className='container mx-auto'>
-            <Plot
-                data={[
-                    {
-                        x: [1,2,3],
-                        y: [3,5,2],
-                        type: "bar"
-                    }
-                ]}
-                layout={{
-                    width: 400,
-                    height: 400,
-                    title: "Plot"
-                }}
-            />
+
+            <div className="flex w-full justify-center pt-3">
+                <Plot
+                    data={[
+                        {
+                            x: [1,2,3],
+                            y: [3,5,2],
+                            type: "bar"
+                        }
+                    ]}
+                    layout={{
+                        width: width,
+                        height: height,
+                        title: "Plot"
+                    }}
+                />
+            </div>
         </div>
     )
 };
