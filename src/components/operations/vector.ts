e9defbfd0183e069c63e036be1199b220a29c938
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-13 05:58:52 -0230

    Defined a hook that adds vector to the data

---
+++ src/components/operations/vector.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+import {DataType, MetaData} from '../types';
+import getRandomColor from '../utilities/randomColor';
+
+// Contains all the vector operations/hooks
+
+// This hook returns a function that simply adds a vector to the data. 
+function useAddVector(
+    setData: React.Dispatch<React.SetStateAction<DataType[]>>,
+    setMetaData: React.Dispatch<React.SetStateAction<MetaData[]>>
+    ): () => void {
+    
+    return () => {
+        const d = new Date();
+        // Add a vector to the data => scatter3d
+        setData((prevData) => [...prevData, {
+            x: [0,1],
+            y: [0,1],
+            z: [0,1],
+            type: "scatter3d",
+            line: {
+                color: getRandomColor()
+            }
+        }]);
+        setMetaData((prev) => [...prev, {
+            id: prev[prev.length-1].id + 1,
+            info: `[${d.getUTCHours()}:${d.getUTCMinutes()}:${d.getUTCSeconds()}.${d.getUTCMilliseconds()}] Vector ${prev[prev.length-1].id + 1} created by the user.`,
+            type: "VECTOR"
+        }]);
+    }
+}
