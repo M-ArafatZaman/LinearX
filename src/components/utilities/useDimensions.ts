f65e187080e31b40c70d3f794ed500ea38de4eaa
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-13 05:36:35 -0230

    Plot dimensions updates

--- src/components/utilities/useDimensions.ts
+++ src/components/utilities/useDimensions.ts
@@ -1,13 +1,16 @@
 import React, {useState, useEffect} from 'react';
 
+const MAX_WIDTH = 700;
+const MAX_HEIGHT = 450;
+
 const useDimensions = () => {
-    const [width, setWidth] = useState<number>( Math.min( window.innerWidth - 20, 900 ) );
-    const [height, setHeight] = useState<number>( Math.min( window.innerHeight - 40, 600 ) );
+    const [width, setWidth] = useState<number>( Math.min( window.innerWidth - 20, MAX_WIDTH ) );
+    const [height, setHeight] = useState<number>( Math.min( window.innerHeight - 40, MAX_HEIGHT ) );
 
     useEffect(() => {
         const onResize = () => {
-            setWidth( Math.min( window.innerWidth - 20, 900 ) );
-            setHeight( Math.min( window.innerHeight - 40, 400 ) );
+            setWidth( Math.min( window.innerWidth - 20, MAX_WIDTH ) );
+            setHeight( Math.min( window.innerHeight - 40, MAX_HEIGHT ) );
         };
 
         window.addEventListener("resize", onResize);
