f40f7660dbabccef7569118591c90a23aaa5a433
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-16 15:17:42 -0230

    Added a home page

---
+++ src/routes/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Layout';
+
+const Home: React.FC = () => {
+    return (
+        <Layout>
+            Test
+        </Layout>
+    )
+};
+
+export default Home;
