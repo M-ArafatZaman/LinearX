807e5a283583946133747c63a772c1cf0507149c
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-16 01:44:22 -0230

    Implemented a hook that returns a function to remove data

--- src/components/operations/index.ts
+++ src/components/operations/index.ts
@@ -1,5 +1,30 @@
+import React from 'react';
 import {useAddVector, useUpdateVector} from './vector';
 import {useAddPlane, useUpdatePlane, useUpdatePlaneCoeff} from './plane';
+import {DataType, MetaData} from '../types';
+
+type SetState<T> = React.Dispatch<React.SetStateAction<T>>
+
+// Hook to get a function to remove data
+export function useRemoveData(setData: SetState<DataType[]>, setMetaData: SetState<MetaData[]>): (id: number) => void {
+    return (id: number) => {
+        // Get index
+        let i = -1;
+        setMetaData(metaData => metaData.filter((d, ind) => {
+            if (d.id == id) {
+                i = ind;
+                return false;
+            }
+            return true;
+        }));
+        setData(data => data.filter((_, ind) => {
+            if (ind == i) {
+                return false;
+            }
+            return true;
+        }));
+    }
+}
 
 export {
     useAddVector,
