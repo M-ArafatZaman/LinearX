317549393291ee95d71ae5afdb60afe4ced5f5c5
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-17 06:04:41 -0230

    Changed name to LinearGPT from LinearX

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -35,7 +35,7 @@ const Header: React.FC = () => {
                             maxHeight: "50px",
                             objectFit: "contain"
                         }} />
-                        <h1 className="text-2xl font-bold ml-2">LinearX</h1>
+                        <h1 className="text-2xl font-bold ml-2">LinearGPT</h1>
 
                         <a className="button ml-auto cursor-pointer uppercase font-bold rounded-md shadow-lg px-3 py-2 bg-slate-700 text-white transition-all hover:bg-slate-500 hover:shadow-2xl active:bg-slate-400" href="https://github.com/M-ArafatZaman/LinearX" target="_blank"><i className="fa-brands fa-github mr-1"></i> Github</a>
                     </div>
