b80eb3ae731f75e21ef0006fbc34470db7dd12d6
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-18 21:32:08 -0230

    Added an image in the home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from '../Layout';
+import LinearGPT_Showcase from '../assets/lineargpt_showcase.png'; 
 
 const Home: React.FC = () => {
     return (
         <Layout>
             <div className="container mx-auto">
                 <div className="grid sm:grid-cols-1 md:grid-cols-2">
-                    <div>Test1</div>
+                    <div className="p-3 flex justify-center">
+                        <img src={LinearGPT_Showcase} style={{objectFit: "contain", width: "75%"}} />
+                    </div>
                     <div>Test2</div>
                 </div>
             </div>
