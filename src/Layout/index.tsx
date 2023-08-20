ad29047641355534505502161bc3e6bd5b05ad4e
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-19 22:06:48 -0230

    Some layout adjustment

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({children}: LayoutProps) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header/>
-            <main className="flex-1 p-4">
+            <main className="flex-1 p-4 flex">
                 {children}
             </main>
             <Footer/>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,11 +5,14 @@ import LinearGPT_Showcase from '../assets/lineargpt_showcase.png';
 const Home: React.FC = () => {
     return (
         <Layout>
-            <div className="container mx-auto">
+            <div className="container mx-auto flex-1">
                 <div className="grid sm:grid-cols-1 md:grid-cols-2">
-                    <div className="p-3 flex justify-center">
+                    {/* Image */}
+                    <div className="p-3 flex justify-center align-middle">
                         <img src={LinearGPT_Showcase} style={{objectFit: "contain", width: "75%"}} />
                     </div>
+
+                    {/* Login */}
                     <div>Test2</div>
                 </div>
             </div>
