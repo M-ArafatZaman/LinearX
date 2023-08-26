dea9928b012325c9a803704884ecbe8412ac5e62
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-08-25 21:59:35 -0230

    Some cleanup

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,10 +2,10 @@ import React, {useState, useEffect, useRef} from 'react';
 import Layout from '../Layout';
 import plotPNG from '../assets/plot.png';
 import {useTransition, useSpringRef, animated} from '@react-spring/web';
+import LoginForm from './home/LoginForm';
 
 const Home: React.FC = () => {
     const [tab, setTab] = useState<string>("login");
-    const [LoginFormHeight, setLoginFormHeight] = useState<number>(0);
 
     const setLogin = () => {
         setTab("login");
@@ -15,33 +15,6 @@ const Home: React.FC = () => {
         setTab("signup");
     };
 
-    // Load google sign in button
-    useEffect(() => {
-        const googleSignInButton = document.getElementById("google-sign-in-button");
-        // @ts-ignore
-        if (googleSignInButton !== null && typeof google !== "undefined") {
-            // @ts-ignore
-            google.accounts.id.initialize({
-                client_id: "626851283273-hgd1l8f503r8dqvu5hde5mmhe3iamjbo.apps.googleusercontent.com",
-                callback: (resp: any) => {console.log(resp)}
-            });
-            // @ts-ignore
-            google.accounts.id.renderButton(
-                googleSignInButton,
-                {
-                    theme: "outline",
-                    size: "large",
-                    text: "continue_with",
-                    type: "standard",
-                    width: "400",
-                    logo_alignment: "center"
-                }
-            );
-        }
-
-    }, []);
-
-    const LoginRef = useRef<HTMLFormElement>(null);
     const transRef = useSpringRef();
     const transitions = useTransition(tab, {
         ref: transRef,
@@ -55,9 +28,6 @@ const Home: React.FC = () => {
         transRef.start();
     }, [tab]);
 
-    useEffect(() => {
-        setLoginFormHeight(typeof LoginRef.current?.clientHeight == "number" ? LoginRef.current.clientHeight : 0);
-    }, [LoginRef]);
     
     return (
         <Layout>
@@ -85,44 +55,7 @@ const Home: React.FC = () => {
                                 {transitions((style, i) => {
                                     return (
                                         <animated.div style={{...style}}>
-                                            <form className="block" id="login-form" ref={LoginRef}>
-                                                {/* Username */}
-                                                <div className="w-full">
-                                                    <div className="my-1 relative rounded-md shadow-md">
-                                                        <input
-                                                        type="text"
-                                                        id="username"
-                                                        name="username"
-                                                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
-                                                        placeholder="Username or Email"
-                                                        />
-                                                    </div>
-                                                </div>
-                                                {/* Password */}
-                                                <div className="w-full">
-                                                    <div className="my-1 relative rounded-md shadow-md">
-                                                        <input
-                                                        type="password"
-                                                        id="password"
-                                                        name="password"
-                                                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
-                                                        placeholder="Password"
-                                                        />
-                                                    </div>
-                                                </div>
-                                                {/* Login submit button */}
-                                                <button type="button" className="uppercase cursor-pointer font-bold rounded-md shadow-lg px-3 py-2 bg-green-600 text-white transition-all hover:bg-green-700 hover:shadow-2xl active:bg-green-200 text-center w-full my-2" >Login</button>
-                                            </form>
-                                            {/* Or use google signup button */}
-                                            <div className="text-sm text-gray-500 my-1 flex flex-row items-center">
-                                                <hr className="flex-1" />
-                                                <span className="mx-2 inline-block">or</span>
-                                                <hr className="flex-1" />
-                                            </div>
-                                            {/* Google sign in button */}
-                                            <div>
-                                                <div id="google-sign-in-button"></div>
-                                            </div>
+                                            <LoginForm/>
                                         </animated.div>
                                     )
                                 })}
