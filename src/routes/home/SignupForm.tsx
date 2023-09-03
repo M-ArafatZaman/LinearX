0eea766bea3117b508e6e6a80089898b465f955b
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-09-02 21:38:49 -0230

    Added the signup form

--- src/routes/home/SignupForm.tsx
+++ src/routes/home/SignupForm.tsx
@@ -1,10 +1,104 @@
-import React, {useState, forwardRef} from 'react';
+import React, {useEffect, useState, forwardRef} from 'react';
 
 const SignupForm = forwardRef<HTMLDivElement>((props, ref: React.ForwardedRef<HTMLDivElement>) => {
 
+    // Load google sign in button
+    useEffect(() => {
+        const googleSignUpButton = document.getElementById("google-sign-up-button");
+        // @ts-ignore
+        if (googleSignUpButton !== null && typeof google !== "undefined") {
+            // @ts-ignore
+            google.accounts.id.initialize({
+                client_id: "626851283273-hgd1l8f503r8dqvu5hde5mmhe3iamjbo.apps.googleusercontent.com",
+                callback: (resp: any) => {console.log(resp)}
+            });
+            // @ts-ignore
+            google.accounts.id.renderButton(
+                googleSignUpButton,
+                {
+                    theme: "outline",
+                    size: "large",
+                    text: "signup_with",
+                    type: "standard",
+                    width: "400",
+                    logo_alignment: "center"
+                }
+            );
+        }
+
+    }, []);     
+
     return (
         <div className="absolute w-full" ref={ref}>
-            Signup form
+            <form className="block" id="signup-form">
+                {/* Name */}
+                <div className="w-full">
+                    <div className="my-1 relative rounded-md shadow-md">
+                        <input
+                        type="text"
+                        name="name"
+                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
+                        placeholder="Name"
+                        />
+                    </div>
+                </div>
+                {/* Username */}
+                <div className="w-full">
+                    <div className="my-1 relative rounded-md shadow-md">
+                        <input
+                        type="text"
+                        name="username"
+                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
+                        placeholder="Username"
+                        />
+                    </div>
+                </div>
+                {/* Email */}
+                <div className="w-full">
+                    <div className="my-1 relative rounded-md shadow-md">
+                        <input
+                        type="email"
+                        name="email"
+                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
+                        placeholder="Email"
+                        />
+                    </div>
+                </div>
+                {/* Password */}
+                <div className="w-full">
+                    <div className="my-1 relative rounded-md shadow-md">
+                        <input
+                        type="password"
+                        name="password"
+                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
+                        placeholder="Password"
+                        />
+                    </div>
+                </div>
+                {/* Confirm Password */}
+                <div className="w-full">
+                    <div className="my-1 relative rounded-md shadow-md">
+                        <input
+                        type="password"
+                        name="confirm-password"
+                        className="focus:border-none block w-full border-gray-300 rounded-md border-none px-3 py-2"
+                        placeholder="Confirm password"
+                        />
+                    </div>
+                </div>
+                {/* Login submit button */}
+                <button type="button" className="uppercase cursor-pointer font-bold rounded-md shadow-lg px-3 py-2 bg-green-600 text-white transition-all hover:bg-green-700 hover:shadow-2xl active:bg-green-200 text-center w-full my-2" >Sign up</button>
+            </form>
+            {/* Or use google signup button */}
+            <div className="text-sm text-gray-500 my-1 flex flex-row items-center">
+                <hr className="flex-1" />
+                <span className="mx-2 inline-block">or</span>
+                <hr className="flex-1" />
+            </div>
+            {/* Google sign up button */}
+            <div>
+                <div id="google-sign-up-button"></div>
+            </div>
         </div>
     )
 });
